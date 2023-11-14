8b213ac0b32b868baf673633f8de2f25707e71eb
AlexJov <aleksjovovic@gmail.com>
2023-11-14 13:12:36 +0000

    feat(model): use custom image and model from bucket instead of predefined

--- infrastructure/model_serving_stack.py
+++ infrastructure/model_serving_stack.py
@@ -1,13 +1,21 @@
 from aws_cdk import (
     Stack,
     RemovalPolicy,
+    CfnOutput,
     aws_iam,
     aws_ssm,
     aws_s3,
-    aws_sagemaker as sagemaker
+    aws_sagemaker as sagemaker,
+    aws_s3_deployment
 )
 
 import logging
+
+# import urllib.request
+# import boto3
+# import botocore
+# import requests
+# import mimetypes
 
 from constructs import Construct
 
@@ -23,20 +31,21 @@ class ModelServingStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, env, model_info, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        #TODO Configuration via CDK + api call for Endpoint
+
         ROLE_NAME = model_info["role_name"] #"SageMakerExecutionRole"
-        MODEL_BUCKET_NAME = model_info["model_bucket_name"] #"sagemaker-llama-model-bucket"
+        #MODEL_BUCKET_NAME = model_info["model_bucket_name"] #"sagemaker-llama-model-bucket"
         REGION_NAME = str(env.region)
-        
-        print("Region: ", REGION_NAME)
 
-        # create bucket
-        bucket = aws_s3.Bucket(self, f"{MODEL_BUCKET_NAME}",
-            versioned=False,
-            removal_policy=RemovalPolicy.DESTROY
-        )
+        INSTANCE_TYPE = "ml.c7g.2xlarge" # make sure you use correct instance types x86 or graviton 
 
-        MODEL_BUCKET_NAME = bucket.bucket_name
-        #print("bucket: ", str(dict(value=bucket.bucket_name)))
+        model_docker_image = "859155805248.dkr.ecr.us-east-1.amazonaws.com/llm-cpu-repository:latest" # inference_image_uri
+        model_bucket_name = "imagebuildingstack-sagemakerllamamodelbucket4b31a-kyl4wi1ntha8"
+        model_bucket_key = "llama-2-7b-chat.tar.gz"
+
+        MODEL_NAME = 'llamacpp-arm64-c7-x8-v00'
+        ENDPOINT_CONFIG_NAME = f'{MODEL_NAME}-config'
+        ENDPOINT_NAME = f'{MODEL_NAME}-stream'
 
         sagemaker_role = aws_iam.Role(self, f"{ROLE_NAME}",
             assumed_by=aws_iam.ServicePrincipal("sagemaker.amazonaws.com")
@@ -95,44 +104,7 @@ class ModelServingStack(Stack):
         sagemaker_role.attach_inline_policy(sts_policy)
         sagemaker_role.attach_inline_policy(logs_policy)
         sagemaker_role.attach_inline_policy(ecr_policy)
-        sagemaker_role.attach_inline_policy(s3_policy)
-
-        MODEL_NAME = 'llamacpp-arm64-c7-x8-v00'
-        #Graviton
-        #MODEL_IMAGE = '444975673530.dkr.ecr.us-east-1.amazonaws.com/y0d1u8z0/y0d1u8z0/llmm-cpu-arm64-full-v00:perf'  #private pull-through cache of public.ecr.aws/y0d1u8z0/llmm-cpu-arm64:latest
-
-        MODEL_IMAGE = "public.ecr.aws/nginx/nginx:alpine-slim"
-
-        ENDPOINT_CONFIG_NAME = f'{MODEL_NAME}-config'
-        ENDPOINT_NAME = f'{MODEL_NAME}-stream'
-
-        INSTANCE_TYPE = "ml.c7g.2xlarge" # make sure you use correct instance types x86 or graviton 
-
-        MODEL_VERSION = "*"  # latest
-        SCOPE = "inference"
-
-        model_id = "huggingface-llm-mistral-7b-instruct" #"meta-textgeneration-llama-2-7b"
-        region_name = str(env.region)
-        instance_type = "ml.g5.24xlarge"
-
-        inference_model_uri = model_uris.retrieve(model_id=model_id, 
-                                          model_version=MODEL_VERSION, 
-                                          model_scope=SCOPE)
-
-        inference_image_uri = image_uris.retrieve(region=region_name, 
-                                          framework=None,
-                                          model_id=model_id, 
-                                          model_version=MODEL_VERSION, 
-                                          image_scope=SCOPE, 
-                                          instance_type=instance_type)
-
-
-        print("inference_model_uri: ", inference_model_uri) 
-        # s3://jumpstart-cache-prod-us-east-1/huggingface-infer/prepack/v1.0.0/infer-prepack-huggingface-llm-mistral-7b-instruct.tar.gz
-
-        model_bucket_name = inference_model_uri.split("/")[2]
-        model_bucket_key = "/".join(inference_model_uri.split("/")[3:])
-        model_docker_image = inference_image_uri
+        sagemaker_role.attach_inline_policy(s3_policy)    
 
         endpoint = SageMakerEndpointConstruct(self, f"{MODEL_NAME}",
                                     project_prefix = "GenerativeAiDemoLlama",
