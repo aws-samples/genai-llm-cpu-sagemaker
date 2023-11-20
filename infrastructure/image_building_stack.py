90e7cd260dae2f35c7f281ced7a6b3b71c3f13f0
AlexJov <aleksjovovic@gmail.com>
2023-11-20 18:55:22 +0000

    feat(image): use asset bucket; export repository uri;

--- infrastructure/image_building_stack.py
+++ infrastructure/image_building_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_ssm,
     aws_s3,
     aws_s3_deployment,
+    aws_s3_assets,
     aws_codebuild,
     aws_codepipeline,
     aws_codepipeline_actions,
@@ -14,60 +15,33 @@ import shutil
 
 class ImageBuildingStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, env, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, project_name: str, repository_name: str, image_bucket_name: str, model_bucket_name: str, env, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        PROJECT_NAME = "llm-cpu"
-        REPOSITORY_NAME = "llm-cpu-repository"
-        IMAGE_BUCKET_NAME = "llm-cpu-bucket"
-        MODEL_BUCKET_NAME = "sagemaker-llama-model-bucket"
-
+        PROJECT_NAME = project_name
+        REPOSITORY_NAME = repository_name
+        IMAGE_BUCKET_NAME = image_bucket_name 
+        MODEL_BUCKET_NAME = model_bucket_name
         ROOT_DIR = os.path.abspath(os.curdir)
 
-        # define the s3 artifact
-        source_output = aws_codepipeline.Artifact(artifact_name='source')
-
-        # create bucket
-        bucket_model = aws_s3.Bucket(self, f"{MODEL_BUCKET_NAME}",
-            versioned=False,
-            removal_policy=RemovalPolicy.RETAIN
+        asset_bucket = aws_s3_assets.Asset(self, "DockerAssets",
+            path = os.path.join(ROOT_DIR, "docker"),
         )
-
-        # pipeline requires versioned bucket
-        bucket = aws_s3.Bucket(
-            self, "SourceBucket",
-            bucket_name=f"{IMAGE_BUCKET_NAME}",
-            versioned=True,
-            removal_policy=RemovalPolicy.DESTROY)
-
-        shutil.make_archive("docker", "zip", os.path.join(ROOT_DIR, "docker"))
-        shutil.copy(os.path.join(ROOT_DIR, "docker.zip"), os.path.join(ROOT_DIR, "docker", "docker.zip"))
-
-        s3_bucket_deployment = aws_s3_deployment.BucketDeployment(self, "DockerFiles",
-            sources=[aws_s3_deployment.Source.asset(os.path.join(ROOT_DIR, "docker"))],
-            destination_bucket=bucket
-        )
-
-        # # ssm parameter to get bucket name later
-        # bucket_param = aws_ssm.StringParameter(
-        #     self, "ParameterB",
-        #     parameter_name=f"{IMAGE_BUCKET_NAME}",
-        #     string_value=bucket.bucket_name,
-        #     description='cdk pipeline bucket')
 
         # ecr repo to push docker container into
         ecr = aws_ecr.Repository(
             self, "ECR",
             repository_name=f"{REPOSITORY_NAME}",
-            removal_policy=RemovalPolicy.DESTROY
+            removal_policy=RemovalPolicy.DESTROY,
+            #auto_delete_images=True,
         )
 
         standard_image = aws_codebuild.LinuxBuildImage.STANDARD_6_0
 
         # codebuild project meant to run in pipeline
-        codebuild_docker_project = aws_codebuild.PipelineProject(
-            self, "DockerBuild",
-            project_name=f"{PROJECT_NAME}-Docker-Build",
+        codebuild_project = aws_codebuild.PipelineProject(
+            self, "PipelineProject",
+            project_name=f"{PROJECT_NAME}-image-build",
             build_spec=aws_codebuild.BuildSpec.from_source_filename(
                 filename='docker_build_buildspec.yml'),
             environment=aws_codebuild.BuildEnvironment(
@@ -79,7 +53,7 @@ class ImageBuildingStack(Stack):
                 "CDK_DEPLOY_ACCOUNT": aws_codebuild.BuildEnvironmentVariable(value=os.getenv('CDK_DEPLOY_ACCOUNT') or ""),
                 "CDK_DEPLOY_REGION": aws_codebuild.BuildEnvironmentVariable(value=os.getenv('CDK_DEPLOY_REGION') or ""),
                 "REPOSITORY_NAME": aws_codebuild.BuildEnvironmentVariable(value=f"{REPOSITORY_NAME}"),
-                "MODEL_BUCKET_NAME": aws_codebuild.BuildEnvironmentVariable(value=f"{bucket_model.bucket_name}"),
+                #"MODEL_BUCKET_NAME": aws_codebuild.BuildEnvironmentVariable(value=f"{bucket.bucket_name}"),
                 "ECR": aws_codebuild.BuildEnvironmentVariable(
                     value=ecr.repository_uri),
                 "TAG": aws_codebuild.BuildEnvironmentVariable(
@@ -88,24 +62,23 @@ class ImageBuildingStack(Stack):
             description='Pipeline to build and push images to ECR',
             timeout=Duration.minutes(60),
         )
-        # codebuild iam permissions to read write s3
-        bucket.grant_read_write(codebuild_docker_project)
-        bucket_model.grant_read_write(codebuild_docker_project)
 
         # codebuild permissions to interact with ecr
-        ecr.grant_pull_push(codebuild_docker_project)
+        ecr.grant_pull_push(codebuild_project)
+
+        source_output = aws_codepipeline.Artifact(artifact_name='source')
 
         pipeline = aws_codepipeline.Pipeline(
             self, "Pipeline",
-            pipeline_name=f"{PROJECT_NAME}",
-            artifact_bucket=bucket,
+            pipeline_name=f"{PROJECT_NAME}-image-build",
+            artifact_bucket=asset_bucket.bucket,
             stages=[
                 aws_codepipeline.StageProps(
                     stage_name='Source',
                     actions=[
                          aws_codepipeline_actions.S3SourceAction(
-                            bucket=bucket,
-                            bucket_key='docker.zip',
+                            bucket=asset_bucket.bucket,
+                            bucket_key=asset_bucket.s3_object_key,
                             action_name='DockerfileSource',
                             run_order=1,
                             output=source_output,
@@ -119,10 +92,17 @@ class ImageBuildingStack(Stack):
                         aws_codepipeline_actions.CodeBuildAction(
                             action_name='DockerBuildImages',
                             input=source_output,
-                            project=codebuild_docker_project,
-                            run_order=1,
+                            project=codebuild_project,
+                            run_order=2,
                         )
                     ]
                 )
             ]
         )
+
+        CfnOutput(scope=self,
+            id="image_repository_uri", 
+            value=ecr.repository_uri, 
+            export_name="var-modelrepositoryuri"
+            )
+
