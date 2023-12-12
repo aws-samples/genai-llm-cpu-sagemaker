bf124abf48f8025fc1ffe19a3e735254b836f34b
Alex Tarasov <alexvt@amazon.co.uk>
2023-12-12 16:59:26 +0000

    gpu-compatible config

--- docker/server.sh
+++ docker/server.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 echo "server.sh"
+
+# Check if NVIDIA GPU is available
+if lspci | grep -i nvidia &> /dev/null; then
+  echo "NVIDIA GPU is available."
+  NGL=999
+else
+  echo "No NVIDIA GPU found."
+  NGL=0
+fi
+
 killall server
-/app/server -m "$1" -c 2048 -t $(nproc --all) --host 0.0.0.0 --port 8081 & > /dev/null
+/app/server -m "$1" -c 2048 -t $(nproc --all) --host 0.0.0.0 --port 8081 -np $(($(nproc --all) / 4)) -ngl $NGL & > /dev/null
