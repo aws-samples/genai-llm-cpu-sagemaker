056f5df640c223f3ca72a2c1bf171d9b7c541deb
Alex Tarasov <alexvt@amazon.co.uk>
2023-12-20 16:47:54 +0000

    dynamic slots

--- docker/server.sh
+++ docker/server.sh
@@ -6,10 +6,12 @@ echo "args: $1"
 if lspci | grep -i nvidia &> /dev/null; then
   echo "NVIDIA GPU is available."
   NGL=999
+  CPU_PER_SLOT=1
 else
   echo "No NVIDIA GPU found."
   NGL=0
+  CPU_PER_SLOT=4
 fi
 
 killall server
-/app/server -m "$1" -c 2048 -t $(nproc --all) --host 0.0.0.0 --port 8081 -cb -np $(($(nproc --all) / 4)) -ngl $NGL &
+/app/server -m "$1" -c 2048 -t $(nproc --all) --host 0.0.0.0 --port 8081 -cb -np $(($(nproc --all) / $CPU_PER_SLOT)) -ngl $NGL &
